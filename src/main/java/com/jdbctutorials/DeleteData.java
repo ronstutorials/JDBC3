5646a1c317ef755b4eb677f74b9e9526b361fab6
Ron Yahav <ronnie@codota.com>
2018-10-28 18:49:36 +0200

    fixed deletedata class (wrong code)
    git pull origin master

--- src/main/java/com/jdbctutorials/DeleteData.java
+++ src/main/java/com/jdbctutorials/DeleteData.java
@@ -5,17 +5,22 @@ import java.sql.*;
 public class DeleteData {
 
     public static void main(String[] args) {
+        
+            String url = "jdbc:mysql://localhost:3306/mydb?autoReconnect=true&useSSL=false";
+            String user = "root";
+            String password = "secret";
+            try {
+                Connection conn = DriverManager.getConnection(url, user, password);
+                Statement stmt = conn.createStatement();
+                int rowsaffected =  stmt.executeUpdate("update Contacts"+" where idContacts = 12");
 
-        String url;
-        String user;
-        String password;
-        Connection conn = DriverManager.getConnection(url, user, password);
-        Statement stmt = conn.createStatement();
-        PreparedStatement ps;
-        ResultSet rs = ps.executeQuery();
+                System.out.println("rows changed: " + rowsaffected);
 
-        while (rs.next())
 
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             
 
 
